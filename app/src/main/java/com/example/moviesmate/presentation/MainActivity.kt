440c02d3bf56c8a881cc85accccb20f311ce79a0
GFO <gia.focxverashvili@gmail.com>
2025-01-16 22:24:17 +0400

    init auth_branch

--- app/src/main/java/com/example/moviesmate/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviesmate/presentation/MainActivity.kt
@@ -1,14 +1,11 @@
 package com.example.moviesmate.presentation
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import com.example.moviesmate.R
 import com.example.moviesmate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
