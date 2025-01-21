13e71a7582551a6bf704ef05f6570ee1a4e19368
GFO <gia.focxverashvili@gmail.com>
2025-01-21 11:38:01 +0400

    add

--- app/src/main/java/com/example/moviesmate/presentation/screens/passwordRecover/forgotPassword/ForgotPasswordFragment.kt
+++ app/src/main/java/com/example/moviesmate/presentation/screens/passwordRecover/forgotPassword/ForgotPasswordFragment.kt
@@ -16,19 +16,12 @@ class ForgotPasswordFragment :
 
     private fun resetPasswordTest(email: String) {
         FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-
     }
 
-    private fun generateVerificationCode(): String {
-        val randomCode = (1000..9999).random()  // Generates a random 4-digit number
-        return randomCode.toString()
-    }
 
     private fun goToVerifyEmailFragment() {
         binding.btnSend.setOnClickListener {
             findNavController().navigate(ForgotPasswordFragmentDirections.actionForgotPasswordFragmentToVerifyEmailFragment())
         }
     }
-
-
 }
